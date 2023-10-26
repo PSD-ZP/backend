df01dc6a83af1d6d722d61188ee82c92ec732c68
Jakub Mikula <jakub.mikula@siemens-healthineers.com>
2023-10-26 21:15:22 +0200

    allow all CORS

--- service/Program.cs
+++ service/Program.cs
@@ -19,13 +19,12 @@ builder.Services.AddControllers(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 
 builder.Services.AddScoped<IWeatherService, WeatherService>();
 
 var app = builder.Build();
-
-
 
 
 // Configure the HTTP request pipeline.
@@ -34,6 +33,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(builder =>
+{
+    builder.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader();
+});
 
 app.UseHttpsRedirection();
 
